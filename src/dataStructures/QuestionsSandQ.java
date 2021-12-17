09d5175bc8ae78d1c744d161beedd16522a8d400
Mohd-Afzal <mohammedafsal878@gmail.com>
2021-12-17 15:59:21 +0530

    Half hour session - Discussed the idea of implementing a stack using two Queues

--- src/dataStructures/QuestionsSandQ.java
+++ src/dataStructures/QuestionsSandQ.java
@@ -2,6 +2,13 @@ package dataStructures;
 
 public class QuestionsSandQ {
 
+    /*
+    *   1. Reverse a stack without using extra space
+    *   2. Reverse a queue
+    *   3. Implement Stack using Queue
+    *   4. Implement Queue using stack
+    * */
+
     public static void main(String[] args) {
         
     }
