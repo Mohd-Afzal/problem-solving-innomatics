dd59c11e6c1867337179d22fdaf4ceab8f100074
Mohd-Afzal <mohammedafsal878@gmail.com>
2021-12-27 09:49:13 +0530

    Printing a Binary Tree in Level Wise Order and Sum of BT

--- src/dataStructures/learningBinaryTree/BinaryTreeUse.java
+++ src/dataStructures/learningBinaryTree/BinaryTreeUse.java
@@ -61,6 +61,71 @@ public class BinaryTreeUse {
         return root;
     }
 
+    public static void printLevelOrder(BinaryTreeNode<Integer> root) {
+        // Write your code here
+
+        // Root check
+        if (root == null) {
+            return;
+        }
+
+        QueueNode<BinaryTreeNode<Integer>> pendingNodes = new QueueNode<>(); // Nodes to process in levels
+        pendingNodes.enqueue(root);
+        pendingNodes.enqueue(null); // ending of the 0th Level
+
+        while (! pendingNodes.isEmpty()) {
+            // Step - 1: Dequeue
+            BinaryTreeNode<Integer> currentNode = pendingNodes.dequeue();
+
+            // Step - 2: Make a decision on printing the data - Done in 2 parts
+            // Part - 1: Check of null, if null then it is the end of the level
+            if (currentNode == null) {
+
+                // Check whether or not null is being repeated on every enqueue operation.
+                if (pendingNodes.isEmpty()) {
+                    return;
+                }
+
+                // End of the current level that we are processing
+                System.out.println();
+
+                // Insert null into the queue to mark the end of the current level
+                pendingNodes.enqueue(null);
+                continue;
+            }
+
+            // Part - 2: If not null -> print the data
+            System.out.print(currentNode.data + " ");
+
+            // Step - 3: Enqueue the Children of the currentNode
+            if (currentNode.leftChild != null)
+                pendingNodes.enqueue(currentNode.leftChild);
+
+            if (currentNode.rightChild != null)
+                pendingNodes.enqueue(currentNode.rightChild);
+        }
+    }
+
+
+    // 450 - 10 20 30 40 50 -1 60 -1 -1 70 -1 80 -1 -1 -1 -1 90 -1 -1
+    public static int getSum(BinaryTreeNode<Integer> root) {
+        // Special Case
+        if (root == null) {
+            return 0;
+        }
+
+        // Base Case
+        if (root.leftChild == null && root.rightChild == null) {
+            return root.data;
+        }
+
+        int leftSum = getSum(root.leftChild);
+        int rightSum = getSum(root.rightChild);
+
+        int myAns = leftSum + rightSum + root.data;
+
+        return myAns;
+    }
 
     public static void main(String[] args) {
 //        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
@@ -76,6 +141,7 @@ public class BinaryTreeUse {
 
         BinaryTreeNode<Integer> root = buildTreeLevelWise();
 //        printLevelOrder(root);
+        System.out.println(getSum(root));
 
     }
 }
